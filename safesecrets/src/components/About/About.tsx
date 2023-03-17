b4e4decc4f9858e9de601c90f4859ba5d30c309d
Bikram Swain <bikramswain1992@gmail.com>
2023-03-17 16:31:29 +0000

    Added about us page.

--- safesecrets/src/components/About/About.tsx
+++ safesecrets/src/components/About/About.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable max-len */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import pwaVideo from '../../assets/pwa.mp4';
+import '../../css/privacypolicy.scss';
 
 const About = () => {
   const navigate = useNavigate();
@@ -13,49 +16,85 @@ const About = () => {
     <div className="about-us-details">
       <h3>About us</h3>
       <p>
-        At
-        <span className="text-clr-primary">&nbsp;SafeSecrets</span>
-        , we are committed to protecting the privacy of our users. This Privacy Policy explains how we collect, use, and share information about you when you use our website https://safesecrets.in.
+        <span className="text-clr-primary">SafeSecrets&nbsp;</span>
+        started with an intent of creating an simple cloud storage for password and/or secrets which can be accessed from any where on any device. It is fast, safe and ensures segration of user data by applying two level of encryption. Double encryption enables safe secret to generate new encryption keys per password per user. The keys are also encrypted to avoid misuse in case anyone gets there hands on it. The entire infrastructure hosting and running
+        <span className="text-clr-primary">&nbsp;SafeSecrets&nbsp;</span>
+        is hosted with in Azure with firewalls and network security which protects againts any unauthorized access to backend. In short we ensure your secret is accessible only to you and no one else!
       </p>
-      <h3>Collection of Information</h3>
+      <h3>Features</h3>
       <p>
-        We collect information about you when you use our website. This may include the following:
-        <br />
-        <ul>
-          <li>Information you provide directly to us: We collect information that you provide to us directly, such as when you sign up for a newsletter or create an account on our website. This information may include your name, email address, and any other information you choose to provide.</li>
-          <li>Information we collect automatically: When you use our website, we may automatically collect information about you, such as your IP address, browser type, and operating system. We may also collect information about your usage of our website, such as the pages you visit and the actions you take.</li>
-        </ul>
-      </p>
-      <h3>Use of Information</h3>
-      <p>
-        We use the information we collect about you for the following purposes:
-        <br />
-        <ul>
-          <li>To provide, maintain, and improve our website: We use the information we collect to provide, maintain, and improve our website and the services we offer. This may include using your information to respond to your requests, to personalize your experience on our website, and to improve the functionality and performance of our website.</li>
-          <li>To communicate with you: We may use the information we collect to communicate with you, such as to send you newsletters or updates about our website.</li>
-          <li>For legal purposes: We may use the information we collect as necessary for legal purposes, such as to comply with our legal obligations, to enforce our policies, and to protect the rights and safety of our users and others.</li>
-        </ul>
-      </p>
-      <h3>Sharing of Information</h3>
-      <p>
-        We do not share your personal information with third parties except in the following circumstances:
-        <ul>
-          <li>With your consent: We may share your information with third parties if you have given us your consent to do so.</li>
-          <li>For legal reasons: We may share your information as necessary to comply with the law, to enforce our policies, or to protect the rights and safety of our users and others.</li>
-        </ul>
-      </p>
-      <h3>Data Security</h3>
-      <p>We take reasonable measures to protect the information we collect from loss, misuse, and unauthorized access, disclosure, alteration, and destruction. However, no method of transmission over the internet, or method of electronic storage, is 100% secure, and we cannot guarantee the absolute security of your information.</p>
-      <h3>Your Choices</h3>
-      <p>
-        You have the following choices regarding the information we collect about you:
+        Save Secrets:
         <br />
         <ul>
           <li>
-            Opt-out of communications: You can opt-out of receiving communications from us by contacting us at&nbsp;
-            <a className="text-clr-primary" href="mailto:contactus@safesecrets.in">contactus@safesecrets.in</a>
+            Login / Signup to
+            <span className="text-clr-primary">&nbsp;SafeSecrets&nbsp;</span>
+            .
+          </li>
+          <li>
+            Click on
+            <strong>&nbsp;Add&nbsp;</strong>
+            button.
+          </li>
+          <li>
+            Fill in secret name and secret value and save.
           </li>
         </ul>
+      </p>
+      <p>
+        Manage Secrets:
+        <br />
+        <ul>
+          <li>Landing page will have options to view or delete the secrets.</li>
+          <li>Simply click on the view icon to view your stored secret.</li>
+          <li>Similarly click on delete icon to delete a secret.</li>
+          <li>
+            Upon viewing a secret two more functionality will be available
+            <strong>&nbsp;Edit&nbsp;</strong>
+            and
+            <strong>&nbsp;Share&nbsp;</strong>
+            .
+          </li>
+          <li>
+            <strong>&nbsp;Edit&nbsp;</strong>
+            can be used to change the secret name and secret value and re-save it.
+          </li>
+          <li>
+            <strong>&nbsp;Share&nbsp;</strong>
+            can be used to share your secret with someone securely.
+          </li>
+          <li>
+            Upon clicking the
+            <strong>&nbsp;Share&nbsp;</strong>
+            button you would need to enter the email of the person you want to share the secret with.
+          </li>
+          <li>
+            <strong>&nbsp;Shared secrets&nbsp;</strong>
+            tab will list all the secrets that you have shared or the secrets that have been shared with you.
+          </li>
+          <li>
+            Here you can view the secrets that have been shared with you.
+          </li>
+          <li>
+            You will also be able to revoke sharing any secret that you had shared before.
+          </li>
+        </ul>
+      </p>
+      <h3>Progressive Web App</h3>
+      <p>
+        We have tried to give mobile app like experience to
+        <strong>&nbsp;SafeSecrets&nbsp;</strong>
+        on mobile devices. Follow the below video to install the app:
+        <br />
+        <br />
+        <video controls width="270" height="380">
+          <source src={pwaVideo} type="video/mp4" />
+        </video>
+      </p>
+      <h3>Support Us</h3>
+      <p>
+        Please give this application a chance, we are sure you will love it. Share you feedback with us at
+        <a className="text-clr-primary" href="mailto:contactus@safesecrets.in">&nbsp;contactus@safesecrets.in</a>
       </p>
       <div className="btn-container-center">
         <button className="btn btn-primary" onClick={goBack}>Back</button>
